8bfc11ec7f31ee283adff7154aaee84a7636ac2f
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2021-12-12 17:01:18 -0800

    update 1.0.3

--- packages/mf-carousel/src/Carousel.svelte
+++ packages/mf-carousel/src/Carousel.svelte
@@ -1,9 +1,12 @@
 <div class="container">
-    <div>Carousel v1.0.2</div>
+    <div>Carousel v1.0.3</div>
 </div>
 
 <style>
     .container {
         padding: 1rem;
+        font-size: 2rem;
+        color: #C84B31;
+        background-color: #ECDBBA;
     }
 </style>
