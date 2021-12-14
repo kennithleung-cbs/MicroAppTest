3edf55839fd80961c0633ee4d5b44959beac1144
Kennith Leung <kennith.leung@cbsinteractive.com>
2021-12-13 17:18:15 -0800

    add nuxt

---
+++ packages/app4/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <modal-trigger></modal-trigger>
+    <giphy-client></giphy-client>
+  </div>
+</template>
+
+<script>
+import GiphyClient from "../../app3/src/GiphyClient.vue";
+import ModalTrigger from "../../app3/src/ModalTrigger.vue";
+
+export default {
+  components: { GiphyClient, ModalTrigger },
+  name: "IndexPage",
+};
+</script>
